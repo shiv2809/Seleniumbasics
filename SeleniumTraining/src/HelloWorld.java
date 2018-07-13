ce880c3ac8005570319d69d833205bad845e04cd
Bhanu Mahesh <bhanumahesh423@gmail.com>
2018-07-13 14:10:22 -0400

    Adding IE browser driver

--- SeleniumTraining/src/HelloWorld.java
+++ SeleniumTraining/src/HelloWorld.java
@@ -12,19 +12,22 @@ public class HelloWorld
 		System.out.println("akjsdhfsa");
 		
 		//loading the web driver into system property
-		System.setProperty("webdriver.gecko.driver", "web_drivers/geckodriver_64bit.exe");
+		//System.setProperty("webdriver.gecko.driver", "web_drivers/geckodriver_64bit.exe");
 
 		//launches Firefox web browser
-		WebDriver webDriver = new FirefoxDriver();
+		//WebDriver webDriver = new FirefoxDriver();
 		
 		//navigate().to() is loading the web page
-		webDriver.navigate().to("http://www.google.com");
+		//webDriver.navigate().to("http://www.google.com");
 		
 		
-		System.setProperty("webdriver.chrome.driver", "web_drivers/chromedriver.exe");
-		WebDriver chromeBrowser = new ChromeDriver();
-		chromeBrowser.get("https://news.google.com/");
+		//System.setProperty("webdriver.chrome.driver", "web_drivers/chromedriver.exe");
+		//WebDriver chromeBrowser = new ChromeDriver();
+		//chromeBrowser.get("https://news.google.com/");
 		
+		System.setProperty("webdriver.ie.driver", "web_drivers/IEDriverServer.exe");
 		WebDriver ieBrowser = new InternetExplorerDriver();
+		ieBrowser.get("http://www.maps.google.com");
+		
 	}
 }
